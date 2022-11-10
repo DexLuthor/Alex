168a9a11a9a23a7c2b04bdb6ced2adda3f16ab2d
alexweinberg <alex.weinberg.uk@gmail.com>
2022-11-10 15:00:45 +0200

    Homework 1

---
+++ src/main/java/Dmitriy.java
@@ -0,0 +1,70 @@
+public class Dmitriy {
+    //main method
+    public static void main(String[] args) throws InterruptedException {
+
+        // WinPane sandbox = new WinPane();
+        // BetterDmitriy betterDmitriy = new BetterDmitriy();
+        // sandbox.add(betterDmitriy);
+
+        //betterDmitriy.shape(69,69,69);
+
+        //betterDmitriy.triangle(70);
+
+        //betterDmitriy.chain(15,4);
+
+        //betterDmitriy.pattern(50,30,60,6);
+
+        //betterDmitriy.pattern2(3,20,90,4);
+
+        //betterDmitriy.betterchain(10,10);
+
+        //double maximum = NerdyDmitriy.max(500, 200);
+
+        // int factorial = NerdyDmitriy.factorial(6);
+
+        //int power = NerdyDmitriy.power(2,3);
+
+        //System.out.println(power);
+
+        //boolean isPalindrome = new NerdyDmitriy().isPalindrome("anna");
+
+        //System.out.println("String is a Palindrome " + isPalindrome);
+
+        //String unicharString = new NerdyDmitriy().unicharString('x',3);
+
+        //System.out.println(unicharString);
+
+        //boolean isUnicharString = new NerdyDmitriy().isUnicharString("aaaa");
+
+        //System.out.println(isUnicharString);
+
+        // int countChar = new NerdyDmitriy().countChar("my name is alex and im a begginer coder",'i');
+
+        //System.out.println(countChar);
+
+        //Lazy Method?
+
+        //int countWords = new NerdyDmitriy().countWords("my name is alex and im a begginer coder");
+
+        //System.out.println(countWords);
+
+        //boolean isDoubleSpace = new NerdyDmitriy().isDoubleSpace("my name is alex and im a  begginer coder");
+
+        //System.out.println(isDoubleSpace);
+
+        //String deleteDoubleSpace = new NerdyDmitriy().deleteDoubleSpace("my name is alex and  im a begginer coder");
+
+        //System.out.println(deleteDoubleSpace);
+
+        //Cheating? This method prints out string regardless.
+
+        //String deleteDoubleSpaceCheat = new NerdyDmitriy().deleteDoubleSpaceCheat("my  name  is alex  and im a begginer coder");
+
+        //System.out.println(deleteDoubleSpaceCheat);
+
+        String dublicateChar = new NerdyDmitriy().dublicateChar("Hello");
+
+        System.out.println(dublicateChar);
+
+    }
+}
---
+++ src/main/java/NerdyDmitriy.java
@@ -0,0 +1,118 @@
+public class NerdyDmitriy {
+
+    public static double max(double a, double b) {
+
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static int factorial(int a) {
+        int f = 1;
+        for (int i = 1; i <= a; i++) {
+            f = f * i;
+        }
+        return f;
+    }
+
+    public static int power(int a, int b) {
+        int p = a;
+        for (int i = 1; i < b; i++) {
+            p = p * a;
+        }
+        return p;
+    }
+
+    public boolean isPalindrome(String a) {
+        String b = "";
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String unicharString(char c, int n) {
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            s += c;
+        }
+        return s;
+    }
+
+    public boolean isUnicharString(String a) {
+        for (int i = 0; i < a.length() - 1; i++) {
+            if (a.charAt(i) != a.charAt(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int countChar(String s, char c) {
+        int d = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) {
+                d += 1;
+            }
+        }
+        return d;
+    }
+
+    public int countWords(String s) {
+        int d = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 32) {
+                d += 1;
+            }
+        }
+        return d + 1;
+    }
+
+    public boolean isDoubleSpace(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 32) {
+                if (s.charAt(i + 1) == 32) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public String deleteDoubleSpace(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 32) {
+                if (s.charAt(i + 1) == 32) {
+                    return s.replace("  ", " ");
+                }
+            }
+        }
+        String b = "String has no double spaces";
+        return b;
+    }
+
+    public String deleteDoubleSpaceCheat(String s) {
+        return s.replace("  ", " ");
+    }
+
+    public String dublicateChar(String s) {
+        String a = "";
+        for (int i = 0; i < s.length(); i++) {
+            a += s.charAt(i) + s.substring(i, i+1);
+
+        }
+        return a;
+    }
+}
+
+
+
+
+
